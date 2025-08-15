aa1f0a3242372b1c20d520a2b215219e60e06a9c
duarteHiago <hiagofdss900@gmail.com>
2025-08-15 15:36:40 -0300

    att arq

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -8,7 +8,7 @@ router.get('/', webhookController.verifyWebhook);
 
 // Receber mensagens (POST)
 router.post('/', (req, res) => {
-  console.log('Recebi um POST no webhook:', req.body);
+  console.log('Recebi um POST no webhook:', JSON.stringify(req.body, null, 2));
   webhookController.receiveMessage(req, res);
 });
 
